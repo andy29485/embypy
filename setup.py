d2678c51dae18937430e6cffd1f485672d51ffc6
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2017-08-15 12:34:15 -0400

    fixed setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+from distutils import util
+
+embypy_objs = util.convert_path('embypy/objects')
+embypy_utils = util.convert_path('embypy/utils')
+print(embypy_utils)
+setup(name='EmbyPy',
+      version='0.1',
+      description='Python API wrapper for emby media browser',
+      author='Andriy Zasypkin',
+      author_email='AndriyZasypkin@gmail.com',
+      url='https://github.com/andy29485/embypy',
+      package_dir = {
+        'embypy': 'embypy',
+        'embypy.utils': embypy_utils,
+        'embypy.objects': embypy_objs
+      },
+      packages=['embypy', 'embypy.objects', 'embypy.utils'],
+)
