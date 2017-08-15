acf2055ecfc51ad4c087b1b994fd46ecbda386e5
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2017-08-15 12:15:42 -0400

    added setup.py

---
+++ embypy/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='EmbyPy',
+      version='0.1',
+      description='Python API wrapper for emby media browser',
+      author='Andriy Zasypkin',
+      author_email='AndriyZasypkin@gmail.com',
+      url='https://github.com/andy29485/embypy',
+      packages=['embypy'],
+)
