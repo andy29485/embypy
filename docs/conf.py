7858fb38231d0f61bd4fdcac5f2bfe183658c086
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2017-09-29 09:59:11 -0400

    fixed typo

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ extensions = ['sphinx.ext.autodoc',
 if os.getenv('READTHEDOCS') == 'True':
   extensions.append('sphinxcontrib.napoleon')
 else:
-extensions.append('sphinx.ext.napoleon')
+  extensions.append('sphinx.ext.napoleon')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
