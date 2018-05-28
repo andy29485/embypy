d3ce57fff6cd2271312cb783c421c1765d856839
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2018-05-28 19:15:07 -0400

    updated setup to use setuptools

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
-from distutils import util
+import os
+from setuptools import setup, convert_path
 
-embypy_objs = util.convert_path('embypy/objects')
-embypy_utils = util.convert_path('embypy/utils')
+embypy_objs = convert_path('embypy/objects')
+embypy_utils = convert_path('embypy/utils')
+
+def read(fname):
+  return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 with open('requirements.txt', 'r') as f:
   requirements = f.readlines()
 
 setup(name='EmbyPy',
-      version='0.4.0.0',
-      setup_requires=['setuptools-markdown'],
-      long_description_markdown_filename='README.md',
-      description='Python API wrapper for emby media browser',
+      version='0.4.1.0',
       author='Andriy Zasypkin',
       author_email='AndriyZasypkin@gmail.com',
-      url='https://github.com/andy29485/embypy',
+      description='Python API wrapper for Emby Media Browser',
+      long_description=read('README.md'),
+      license='LGPLv3',
+      keywords = 'Emby MediaBrowser API',
+      url='https://pypi.org/project/EmbyPy/',
       package_dir = {
         'embypy': 'embypy',
         'embypy.utils': embypy_utils,
@@ -24,4 +28,11 @@ setup(name='EmbyPy',
       },
       install_requires=requirements,
       packages=['embypy', 'embypy.objects', 'embypy.utils'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: AsyncIO',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    ],
 )
