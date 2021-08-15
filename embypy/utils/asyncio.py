80eea6f68160ef51d280d7ab113522b100aee3b3
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2021-08-15 11:43:05 -0400

    added async gen support for async run wrapper

--- embypy/utils/asyncio.py
+++ embypy/utils/asyncio.py
@@ -25,18 +25,38 @@ def async_func(func):
     return tmp_func
 
 
+def _get_loop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError:
+        return asyncio.new_event_loop()
+
+
+def iter_over_async(ait, loop):
+    ait = ait.__aiter__()
+    async def get_next():
+        try:
+            obj = await ait.__anext__()
+            return False, obj
+        except StopAsyncIteration:
+            return True, None
+    while True:
+        done, obj = loop.run_until_complete(get_next())
+        if done:
+            break
+        yield obj
+
+
 def _run_func(func, *args, **kwargs):
+    out = None
+    if callable(func):
+        out = func(*args, **kwargs)
     if is_asyncio_context():
-        return func(*args, **kwargs)
+        return out
+    elif inspect.isasyncgen(out):
+        with _loop_lock:
+            return iter_over_async(out, _get_loop())
     elif inspect.iscoroutinefunction(func):
         with _loop_lock:
-            loop = None
-            try:
-                loop = asyncio.get_event_loop()
-            except RuntimeError:
-                loop = asyncio.new_event_loop()
-            return loop.run_until_complete(
-                func(*args, **kwargs)
-            )
-    elif callable(func):
-        return func(*args, **kwargs)
+            return _get_loop().run_until_complete(out)
+    return out
